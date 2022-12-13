50c273abd08b2fb26f65abd0eb14e62ed2f41b04
Marcos Vidal <marcosvidal@outlook.com>
2022-12-13 08:14:17 -0300

    initial commit

---
+++ balta-app/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace balta_app;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ balta-app/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace balta_app;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
